57b832918c8f69094b5956e9860810988282c544
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2021-12-03 19:27:29 +0530

    Initial repo

---
+++ main.js
@@ -0,0 +1,6 @@
+const dateOfBirth = document.querySelector("#dob");
+const luckyNum = document.querySelector("#lucky-num");
+const checkBtn = document.querySelector("#check-btn");
+
+checkBtn.addEventListener('click', checkDOB)
+
