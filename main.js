4101c933a5ddf013bf22342ff9b6d065354b9e02
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2021-12-04 11:03:22 +0530

    update main.js

--- main.js
+++ main.js
@@ -2,5 +2,14 @@ const dateOfBirth = document.querySelector("#dob");
 const luckyNum = document.querySelector("#lucky-num");
 const checkBtn = document.querySelector("#check-btn");
 
-checkBtn.addEventListener('click', checkDOB)
+function sumOfDOB() {
+  let sum = 0;
+  let dob = dateOfBirth.value;
+  dob = dob.replaceAll("-", "");
+  for (let i = 0; i < dob.length; i++) {
+    sum = sum + Number(dob.charAt(i));
+  }
+  console.log(sum);
+}
 
+checkBtn.addEventListener("click", sumOfDOB);
